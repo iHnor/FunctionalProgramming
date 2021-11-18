66c53b58e5b9c3c42a15d1ada5d39e84f315546a
Ihor <mixnoiv@gmail.com>
2021-11-18 23:19:29 +0200

    check color

--- functional.js
+++ functional.js
@@ -7,3 +7,4 @@ let shapes = [
 
 const and = (func_1, func_2) => value => func_1(value) && func_2(value);
 const or = (func_1, func_2) => value => func_1(value) || func_2(value);
+const hasColor = c => r => r.color == c;
