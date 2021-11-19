7f57e6d2d4459bf7862fafabd789d1d9c7b5b5e6
Ihor <igormykhno@gmail.com>
2021-11-19 15:50:43 +0200

    add test value

--- functional.js
+++ functional.js
@@ -1,9 +1,13 @@
 let shapes = [
-    { color: 'black', width: 20, height: 20 },
+    { color: 'black', width: 10, height: 10 },
     { color: 'red', width: 12, height: 15 },
-    { color: 'red', width: 4, height: 13 },
-    { color: 'white', width: 23, height: 34 }
-]
+    { color: 'red', width: 6, height: 6 },
+    { color: 'red', width: 6, height: 10 },
+    { color: 'black', width: 12, height: 6 },
+    { color: 'white', width: 23, height: 34 },
+    { color: 'black', width: 5, height: 5 },
+    { color: 'yellow', width: 23, height: 23 }
+];
 
 const and = (func_1, func_2) => value => func_1(value) && func_2(value);
 const or = (func_1, func_2) => value => func_1(value) || func_2(value);
