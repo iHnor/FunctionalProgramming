b33e9ad928d74aa0467f480a33bdcb8339579ebe
Ihor <igormykhno@gmail.com>
2021-11-22 12:03:52 +0200

    refactor methods

--- functional.js
+++ functional.js
@@ -8,30 +8,44 @@ let shapes = [
     { color: 'black', width: 5, height: 5 },
     { color: 'yellow', width: 23, height: 23 }
 ];
+const hasColor = c => figure => figure.color == c;
 
-const and = (func_1, func_2) => value => func_1(value) && func_2(value);
+const argsToArray = fn => (...args) => fn(args)
+
+// Identity Value
+// a + 0 = a
+// a * 1 = a
+// a.concat([])  = a
+// a.concat('')  = a
+// a || false = a
+// a && true = a
+// and(a, _ => true) = a
+// or(a, _ => false) = a
+// identity = a => a
+
+const filter = predicate => figures => figures.filter(predicate);
+const map = projection => list => list.map(projection);
+const reduce = (reducer, initValue) => list => list.reduce(reducer, initValue);
+
+const and = (p1, p2) => value => p1(value) && p2(value);
 const or = (func_1, func_2) => value => func_1(value) || func_2(value);
 const all = argsToArray(reduce(and));
 const any = argsToArray(reduce(or));
-const any = list => list.reduce((func_1, func_2) => value => func_1(value) || func_2(value));
 
-const hasColor = c => figure => figure.color == c;
 const isSquare = figure => figure.width == figure.height;
 const isRectangle = figure => figure.width != figure.height;
 
 const calcArea = figure => figure.width * figure.height;
 const calcPerimeter = figure => (figure.width + figure.height) * 2;
 
+
 const add = (a, b) => a + b;
-const max = list => list.reduce((a, b) => Math.max(a, b));
-const sum = list => list.reduce(add, 0);
+const max = reduce((a, b) => Math.max(a, b), -Infinity);
+const sum = reduce(add, 0)
 
-const map = value => list => list.map(value);
-const reduce = (func, value) => list => list.reduce(func, value);
-const filter = func => figures => figures.filter(func);
-
-const flow = (...listValue) => value => listValue.reduce((acc , func) => func(acc), value);
-
+const flow = (...steps) => value => steps.reduce((result , step) => step(result), value);
+const combine = (...listValue) => value => listValue.reduceRight((acc , func) => func(acc), value);
+const combineFlow = (...steps) => flow(...steps.reverse());
 
 let Red = 'red';
 let sumOfPerimetersRedRectangles = flow(
@@ -49,3 +63,10 @@ let maxAreaFromAllBlackSquare = flow(
     max
 )(shapes)
 console.log("Max area", maxAreaFromAllBlackSquare);
+
+let maxAreaFromAllBlackSquareRevers = combineFlow(
+    max,
+    map(calcArea),
+    filter(and(hasColor(Black), isSquare))
+)(shapes)
+console.log("Max area", maxAreaFromAllBlackSquareRevers);
